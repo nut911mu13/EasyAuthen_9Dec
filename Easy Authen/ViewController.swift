4117137b9f02d5b12808dc7731f849519e9cccba
nut <nut911mu13@gmail.com>
2017-12-10 09:54:09 +0700

    Get Value From TextField Success

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -11,7 +11,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    Explicit
+    var userString: String = ""
+    var passwordString: String = ""
+    var displayStrings = ["Have Space Please Fill All Blank","User False","Welcome Member","Password False"]
+    
+    @IBOutlet weak var userTextField: UITextField!
 
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var displayTextField: UILabel!
+    
+    
+
+    @IBAction func loginButton(_ sender: Any) {
+        
+//        Get Value From TextField
+        userString = userTextField.text!
+        passwordString = passwordTextField.text!
+        
+//        Show Log
+        print("User ==> \(userString)")
+        print("Password ==> \(passwordString)")
+        
+    } // loginButton
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
